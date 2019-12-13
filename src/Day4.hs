ec0aca9f741ef28a8895e258db07355df1c105e3
Joshua Brot <jbrot@umich.edu>
2019-12-12 19:54:52 -0500

    Completed Day 4.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

---
+++ src/Day4.hs
@@ -0,0 +1,42 @@
+module Day4 (day4) where
+
+import Options.Applicative (Parser, flag, short, long, help)
+
+import CLI
+
+foldlDigits :: (a -> Int -> a) -> a -> Int -> a
+foldlDigits f a n
+  | n < 0     = foldlDigits f a (-n)
+  | n < 10    = f a n
+  | otherwise = f (foldlDigits f a (n `div` 10)) (n `mod` 10)
+
+hasAdjacentDigits :: Int -> Bool
+hasAdjacentDigits = snd . foldlDigits (\(n, b) n' -> (n', b || n == n')) (-1, False)
+
+doubleDigits :: Int -> Bool
+doubleDigits = snd . flip step (-1) . foldlDigits step ((-1,-2,-3), False)
+  where step ((n1, n2, n3), b) n4 = ((n2, n3, n4), b || (n1 /= n2 && n2 == n3 && n3 /= n4))
+
+nonDecreasing :: Int -> Bool
+nonDecreasing = snd . foldlDigits (\(n, b) n' -> (n', b && n <= n')) (-1, True)
+
+filteredRange :: (Int, Int) -> [Int]
+filteredRange (l,h) = filter (\x -> hasAdjacentDigits x && nonDecreasing x) [l..h]
+
+filteredRange2 :: (Int, Int) -> [Int]
+filteredRange2 (l,h) = filter (\x -> doubleDigits x && nonDecreasing x) [l..h]
+
+star1 :: IO ()
+star1 = (print . length . filteredRange) =<< readLn
+
+star2 :: IO ()
+star2 = (print . length . filteredRange2) =<< readLn
+
+runStar :: Bool -> IO ()
+runStar False = star1
+runStar True = star2
+
+parseStar :: Parser Bool
+parseStar = flag False True $ short '2' <> long "star2" <> help "Run star2" 
+
+day4 = AnyDay (Day parseStar runStar "day4" "Day 4 of AoC 2019")
--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,10 @@ import CLI
 import Day1
 import Day2
 import Day3
+import Day4
 
 days :: [AnyDay]
-days = [ day1, day2, day3 ]
+days = [ day1, day2, day3, day4 ]
 
 parser :: ParserInfo (IO ())
 parser = info (helper <*> p) (progDesc "This program is designed to solve the 2019 Advent of Code")
